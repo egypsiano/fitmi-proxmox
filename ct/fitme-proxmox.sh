b7537d51d109277aa325d3f2539b03b2240c6e62
egypsiano <egypsiano@users.noreply.github.com>
2025-06-09 23:21:36 +0300

    Add files via upload

--- ct/fitme-proxmox.sh
+++ ct/fitme-proxmox.sh
@@ -13,6 +13,7 @@ function variables() {
 function color() {
     RED='\033[0;31m'
     GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
     NC='\033[0m'
     BOLDGREEN='\033[1;32m'
     BOLDRED='\033[1;31m'
